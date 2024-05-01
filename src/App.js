6eba9546549775c3f27e0affacb4c9a65ff2e999
faiqfisol <faiqfisol@airasia.com>
2024-05-01 21:53:30 +0800

    FEAT: Add improvements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+
 import ResponsiveDrawer from "./Drawer";
 
 function App() {
   const [sales, setSalesReport] = useState([]);
+  const [rowSelectionModel, setRowSelectionModel] = useState([]);
+  const [age, setAge] = useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
   useEffect(() => {
     fetch("/api/Reports")
@@ -16,10 +29,11 @@ function App() {
       });
   }, []);
 
+  console.log("rowselectionmode", rowSelectionModel);
+
   const columns = [
-    { field: "id", headerName: "ID", width: 70 },
     { field: "date", headerName: "Date", width: 130 },
-    { field: "documentId", headerName: "DocumentId", width: 130 },
+    { field: "documentId", headerName: "Document ID", width: 130 },
     { field: "grossMargin", headerName: "Gross Margin", width: 130 },
     { field: "salesVolume", headerName: "Sales Volume", width: 130 },
     { field: "liquidTypeId", headerName: "Liquid Type Id", width: 130 },
@@ -31,17 +45,48 @@ function App() {
       <header className="App-header">
         <ResponsiveDrawer
           children={
-            <DataGrid
-              rows={sales}
-              columns={columns}
-              initialState={{
-                pagination: {
-                  paginationModel: { page: 0, pageSize: 5 },
-                },
-              }}
-              pageSizeOptions={[5, 10]}
-              checkboxSelection
-            />
+            <>
+              <Box component="form" noValidate autoComplete="off" display={"flex"} pb={2}>
+                <Box pr={2}>
+                  <TextField id="outlined-basic" label="Search" variant="outlined" />
+                </Box>
+                <Box sx={{ minWidth: "200px" }}>
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Liquid Type</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={age}
+                      label="Liquid Type"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={"RON95"}>RON95</MenuItem>
+                      <MenuItem value={"RON97"}>RON97</MenuItem>
+                      <MenuItem value={"DIESEL"}>DIESEL</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Box>
+
+              <DataGrid
+                rows={sales}
+                columns={columns}
+                initialState={{
+                  pagination: {
+                    paginationModel: { page: 0, pageSize: 5 },
+                  },
+                }}
+                pageSizeOptions={[5, 10, 20]}
+                onRowSelectionModelChange={(newRowSelectionModel) => {
+                  console.log("newRowSelectionModel", newRowSelectionModel);
+                  setRowSelectionModel(newRowSelectionModel);
+                }}
+                rowSelectionModel={rowSelectionModel}
+                checkboxSelection
+                disableRowSelectionOnClick
+                autoHeight
+              />
+            </>
           }
         />
       </header>
--- src/Drawer.js
+++ src/Drawer.js
@@ -5,13 +5,12 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
+import AssessmentIcon from "@mui/icons-material/Assessment";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -42,25 +41,12 @@ function ResponsiveDrawer({ children }) {
       <Toolbar />
       <Divider />
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        <ListItem key={"Dashboard"} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>{<AssessmentIcon />}</ListItemIcon>
+            <ListItemText primary={"Dashboard"} />
+          </ListItemButton>
+        </ListItem>
       </List>
     </div>
   );
@@ -87,7 +73,7 @@ function ResponsiveDrawer({ children }) {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
-            Responsive drawer
+            PETRONAS Sales Report
           </Typography>
         </Toolbar>
       </AppBar>
